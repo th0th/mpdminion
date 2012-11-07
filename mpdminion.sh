8d14b914884a2310132d39305b05fecbc9516501
th0th <me@th0th.me>
2012-11-07 23:07:34 +0200

    remove forgotten line

--- mpdminion.sh
+++ mpdminion.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 CMD_DMENU='dmenu -i'
-NUMBER_OF_SONGS=$(mpc playlist | wc -l)
 SONG=$(mpc playlist | $CMD_DMENU)
 
 SONG_NO=$(mpc playlist | grep -n "^$SONG$" | sed -e 's/\(.*\):.*/\1/')
